1381d5c1af6163c9eeea9a27a8972f450f2c7897
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-16 21:36:13 +0600

    next, prisma, next-auth project setup

---
+++ pages/p/[id].tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { GetServerSideProps } from "next"
+import ReactMarkdown from "react-markdown"
+import Layout from "../../components/Layout"
+import { PostProps } from "../../components/Post"
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: String(params?.id),
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  
+  return {
+    props: {post},
+  };
+}
+
+const Post: React.FC<{post: PostProps}> = (props) => {
+  let title = props?.post?.title
+  if (!props?.post?.published) {
+    title = `${title} (Draft)`
+  }
+  
+
+  return (
+    <Layout>
+      <div>
+        <h2>{title}</h2>
+        <p>By {props?.post?.author?.name || "Unknown author"}</p>
+        <ReactMarkdown>
+          {props?.post?.content}
+          </ReactMarkdown>
+      </div>
+      <style jsx>{`
+        .page {
+          background: white;
+          padding: 2rem;
+        }
+
+        .actions {
+          margin-top: 2rem;
+        }
+
+        button {
+          background: #ececec;
+          border: 0;
+          border-radius: 0.125rem;
+          padding: 1rem 2rem;
+        }
+
+        button + button {
+          margin-left: 1rem;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Post
