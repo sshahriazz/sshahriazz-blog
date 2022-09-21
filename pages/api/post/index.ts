bd7e03c7ae5a1961e7a60126db5fa6480b7bd2ee
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-22 00:28:28 +0600

    author removed

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,11 +1,11 @@
-import { getSession } from 'next-auth/react';
-import { NextRequest } from 'next/server';
-import prisma from '../../../lib/prisma';
+import { getSession } from "next-auth/react";
+import { NextRequest } from "next/server";
+import prisma from "../../../lib/prisma";
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req: any, res: any ) {
+export default async function handle(req: any, res: any) {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
@@ -13,7 +13,7 @@ export default async function handle(req: any, res: any ) {
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email! } },
+      // author: { connect: { email: session?.user?.email! } },
     },
   });
   res.json(result);
