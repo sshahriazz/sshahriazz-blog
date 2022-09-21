6b42d3638cd3ee45a904a8d43969e482230a1f17
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-22 00:26:40 +0600

    auth halted

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -9,14 +9,14 @@ import { Box } from "../primitive/Box";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req });
-  if (!session) {
-    res.statusCode = 403;
-    return { props: { drafts: [] } };
-  }
+  // if (!session) {
+  //   res.statusCode = 403;
+  //   return { props: { drafts: [] } };
+  // }
 
   const drafts = await prisma.post.findMany({
     where: {
-      author: { email: session.user?.email },
+      // author: { email: session.user?.email },
       published: false,
     },
     include: {
