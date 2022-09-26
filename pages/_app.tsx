9cbdf63f0456b96d7ef3fd47664a064d27111f8b
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-26 20:32:07 +0600

    removed next-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,7 @@
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import Layout from "../components/Layout";
-import ThemeProvider from "../components/ThemeProvider";
-import { Router } from "next/router";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
-NProgress.configure({
-  showSpinner: false,
-});
-Router.events.on("routeChangeStart", (url) => {
-  NProgress.start();
-});
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <ThemeProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </SessionProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from "next";
-import { Box } from "../primitive/Box";
 
 const Home: NextPage = (props) => {
   return (
-    <Box>
-      <div className="page">
-        <h1>Welcome</h1>
-        <main></main>
-      </div>
-    </Box>
+    <div className="page">
+      <h1>Welcome</h1>
+    </div>
   );
 };
 
