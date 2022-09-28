3d6b494d6096cbef3baf8aac45c3dfdeb6ba5fe9
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2022-09-28 21:39:59 +0600

    home page dummy content added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['ui.mantine.dev']
+  }
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import ContactSection from "components/home/ContactSection";
+import HeroSection from "components/home/HeroSection";
+import { ProjectSection } from "components/home/ProjectSection";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <>Good thing is gonna happen soon</>;
+  return <><HeroSection/> <ProjectSection/> <ContactSection/> </>;
 };
 
 export default Home;
